333b48a6843cef6f6aed15e38d7962aa35e39f03
Mitho Mizuno <mithomizuno@gmail.com>
2024-06-03 10:57:21 +0700

    Complete modification

--- binary/stream.py
+++ binary/stream.py
@@ -1,6 +1,39 @@
 import struct
 from typing import Optional
 
+
+class ByteOrder:
+    LITTLE_ENDIAN = '<'
+    BIG_ENDIAN = '>'
+
+
+class ByteType:
+    SIGNED_CHAR = 'b'
+    UNSIGNED_CHAR = 'B'
+
+    SIGNED_SHORT = 'h'
+    UNSIGNED_SHORT = 'H'
+
+    SIGNED_INT = 'i'
+    UNSIGNED_INT = 'I'
+
+    SIGNED_LONG = 'q'
+    UNSIGNED_LONG = 'Q'
+
+    FLOAT = 'f'
+    DOUBLE = 'd'
+    BOOLEAN = '?'
+
+
+class ByteSize:
+    SHORT = 2
+    INT = 4
+    LONG = 8
+
+    FLOAT = 4
+    DOUBLE = 8
+
+
 class Buffer:
     def __init__(self, buffer: bytes = bytes(), offset: int = 0):
         self.buffer = buffer
@@ -28,13 +61,92 @@ class Buffer:
 
         return buffer
 
-    def write(self, buffer: bytes) -> None:
-        self.buffer += buffer
+    def write(self, __bytes: bytes) -> None:
+        self.buffer += __bytes
 
 
 class BinaryStream(Buffer):
+
     def read_bool(self) -> bool:
-        return struct.unpack("?", self.read(1))[0]
+        return struct.unpack(ByteType.BOOLEAN, self.read(1))[0]
 
     def write_bool(self, value: bool) -> None:
-        self.write(struct.pack("?", value))
+        self.write(struct.pack(ByteType.BOOLEAN, value))
+
+    def read_byte(self, *, unsigned: bool = True, order: str = ByteOrder.BIG_ENDIAN) -> int:
+        __format = order + (ByteType.UNSIGNED_CHAR if unsigned else ByteType.SIGNED_CHAR)
+        return struct.unpack(__format, self.read(ByteSize.SHORT))[0]
+
+    def write_byte(self, value: int, *, unsigned: bool = True, order: str = ByteOrder.BIG_ENDIAN) -> None:
+        __format = order + (ByteType.UNSIGNED_CHAR if unsigned else ByteType.SIGNED_CHAR)
+        self.write(struct.pack(__format, value))
+
+    def read_short(self, *, unsigned: bool = True, order: str = ByteOrder.BIG_ENDIAN) -> int:
+        __format = order + (ByteType.UNSIGNED_SHORT if unsigned else ByteType.SIGNED_SHORT)
+        return struct.unpack(__format, self.read(ByteSize.SHORT))[0]
+
+    def write_short(self, value: int, *, unsigned: bool = True, order: str = ByteOrder.BIG_ENDIAN) -> None:
+        __format = order + (ByteType.UNSIGNED_SHORT if unsigned else ByteType.SIGNED_SHORT)
+        self.write(struct.pack(__format, value))
+
+    def read_triad(self, *, unsigned: bool = True, order: str = ByteOrder.BIG_ENDIAN) -> int:
+        __bytes = self.read(3)
+        __format = order + (ByteType.UNSIGNED_INT if unsigned else ByteType.UNSIGNED_INT)
+        __buffer = b"\x00" + __bytes if unsigned else (b"\x00" if __bytes[2] < 0x80 else b"\xff") + __bytes
+        return struct.unpack(__format, __buffer)[0]
+
+    def write_triad(self, value: int, *, unsigned: bool = True, order: str = ByteOrder.BIG_ENDIAN) -> None:
+        __format = order + (ByteType.UNSIGNED_INT if unsigned else ByteType.UNSIGNED_INT)
+        self.write(struct.pack(__format, value)[1:])
+
+    def read_int(self, *, unsigned: bool = True, order: str = ByteOrder.BIG_ENDIAN) -> int:
+        __format = order + (ByteType.UNSIGNED_INT if unsigned else ByteType.SIGNED_INT)
+        return struct.unpack(__format, self.read(ByteSize.INT))[0]
+
+    def write_int(self, value: int, *, unsigned: bool = True, order: str = ByteOrder.BIG_ENDIAN) -> None:
+        __format = order + (ByteType.UNSIGNED_INT if unsigned else ByteType.SIGNED_INT)
+        self.write(struct.pack(__format, value))
+
+    def read_long(self, *, unsigned: bool = True, order: str = ByteOrder.BIG_ENDIAN) -> int:
+        __format = order + (ByteType.UNSIGNED_LONG if unsigned else ByteType.SIGNED_LONG)
+        return struct.unpack(__format, self.read(ByteSize.LONG))[0]
+
+    def write_long(self, value: int, *, unsigned: bool = True, order: str = ByteOrder.BIG_ENDIAN) -> None:
+        __format = order + (ByteType.UNSIGNED_LONG if unsigned else ByteType.SIGNED_LONG)
+        self.write(struct.pack(__format, value))
+
+    def read_float(self, *, unsigned: bool = True, order: str = ByteOrder.BIG_ENDIAN) -> int:
+        __format = order + (ByteType.FLOAT if unsigned else ByteType.FLOAT)
+        return struct.unpack(__format, self.read(ByteSize.FLOAT))[0]
+
+    def write_float(self, value: int, *, unsigned: bool = True, order: str = ByteOrder.BIG_ENDIAN) -> None:
+        __format = order + (ByteType.FLOAT if unsigned else ByteType.FLOAT)
+        self.write(struct.pack(__format, value))
+
+    def read_double(self, *, unsigned: bool = True, order: str = ByteOrder.BIG_ENDIAN) -> int:
+        __format = order + (ByteType.DOUBLE if unsigned else ByteType.DOUBLE)
+        return struct.unpack(__format, self.read(ByteSize.DOUBLE))[0]
+
+    def write_double(self, value: int, *, unsigned: bool = True, order: str = ByteOrder.BIG_ENDIAN) -> None:
+        __format = order + (ByteType.DOUBLE if unsigned else ByteType.DOUBLE)
+        self.write(struct.pack(__format, value))
+
+    def read_var_int(self):
+        value: int = 0
+        for i in range(0, 35, 7):
+            number = self.read_byte()
+            value |= ((number & 0x7f) << i)
+            if (number & 0x80) == 0:
+                return value
+        raise ValueError("VarInt is too big")
+
+    def write_var_int(self, value: int) -> None:
+        value &= 0xffffffff
+        for i in range(0, 5):
+            to_write: int = value & 0x7f
+            value >>= 7
+            if value != 0:
+                self.write_byte(to_write | 0x80)
+            else:
+                self.write_byte(to_write)
+                break
